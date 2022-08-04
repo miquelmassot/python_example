22ce0989da068accc1401aa0686ac813fa6cb95b
Miquel Massot <miquel.massot@gmail.com>
2022-08-04 10:15:28 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from package_name.module_name import ExampleClass
+
+def main():
+    c = ExampleClass()
+    c.print()
+
+if __name__ == '__main__':
+    main()
---
+++ package_name/module_name.py
@@ -0,0 +1,7 @@
+
+class ExampleClass:
+    def __init__(self):
+        print("Example class created!")
+
+    def print(self):
+        print("Hello world!")
